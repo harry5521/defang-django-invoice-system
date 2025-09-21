423d5c9050606fe74b08a2fa6e87b04f18b888fa
Huraira Gulfaraz <hurairagulfaraz4@gmail.com>
2025-09-21 17:07:01 +0500

    added static root

--- invoice_app/settings.py
+++ invoice_app/settings.py
@@ -154,9 +154,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # optional if you have extra static folders
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    # required for collectstatic
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
